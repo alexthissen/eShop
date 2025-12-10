a423999949a054a33d9cb73494bf52440333b84a
alexthissen <athissen@killer-apps.nl>
2025-12-10 21:08:59 +0000

    Changes for dev containers and arm64 support

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,8 @@
+
+sudo dotnet workload update 
+sudo dotnet workload restore
+
+dotnet tool restore
+
+sudo apt-get update && sudo apt-get install -y protobuf-compiler
+#dotnet dev-certs https --trust

9cb8209ec90e17efea08effcbfcd19efe1255db7
alexthissen <athissen@killer-apps.nl>
2026-05-22 18:07:07 +0000

    Initial commit for template

---
+++ src/dotnetworkload/.devcontainer/post-create.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+sudo dotnet workload update 
+sudo dotnet workload restore
+
+dotnet tool restore
+
+sudo apt-get update && sudo apt-get install -y protobuf-compiler
+
+# Import and trust the ASP.NET Core HTTPS development certificate
+dotnet dev-certs https --import /https/eshop-ssl.pfx --clean --password $PFX_PASSWORD --verbose
+dotnet dev-certs https --trust --verbose
+dotnet dev-certs https --check --trust --verbose
+
+devproxy cert ensure
+sudo openssl pkcs12 -in $HOME/.config/dev-proxy/rootCert.pfx -nokeys -nodes \
+  -out /usr/local/share/ca-certificates/devproxy-ca.crt -passin pass:
+sudo update-ca-certificates
+
+installMinikube=${templateOption:installMinikube}
+
+# Start Minikube with Docker driver and expose port 8081
+if ${installMinikube}; then 
+    minikube start --driver=docker --container-runtime=cri-o
+
+    # Wait for Minikube to be ready
+    while (! kubectl cluster-info ); do
+    echo "Waiting for Minikube to launch..."
+    sleep 2
+    done
+fi
+
+az extension add --name resource-graph
+az extension list
